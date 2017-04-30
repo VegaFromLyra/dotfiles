81cff972287f6494d0a24c99ab433279ffddb7a4
ashab <asha.ab@gmail.com>
2017-04-30 16:33:31 -0400

    Remove extra dir and add alias

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$(  cd "$ (dirname "${BASH_SOURCE[0]}" )" && pwd )"
-EXTRA_DIR="$HOME/.extra"
 
 # Update dotfiles itself first
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
