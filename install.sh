c376cac6e2a08f7f6042b872ea5d0e1ec2adb275
ashab <asha.ab@gmail.com>
2017-10-08 20:18:51 -0400

    Rename
    - I got the name `runcom` from a reference dotfile set up, not sure what
    it meant so renamed it so that at least my future self understands it
    - Updated the install script to use the udpated directory as well

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ echo "Dotfiles: $DOTFILES_DIR"
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Set up symlinks
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/shell/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
-ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
+ln -sfv "$DOTFILES_DIR/shell/.zshrc" ~
